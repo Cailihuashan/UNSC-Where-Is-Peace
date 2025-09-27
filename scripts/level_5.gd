e248f3dc61cd659b0fa36c9fabbe7089cb200550
Cailihuashan <thetruegamer404@gmail.com>
2025-09-27 18:20:32 -0400

    Imported and made the backgrounds,

--- scripts/level_5.gd
+++ scripts/level_5.gd
@@ -5,22 +5,23 @@ const VETO_HAND_SCENE = preload("res://scenes/veto_hand.tscn")
 
 
 func _on_missile_timer_timeout():
-	var screen_size = get_viewport_rect().size
+	var player = $Player
 	var missile = MISSILE_SCENE.instantiate()
-	missile.position = Vector2(randf_range(0, screen_size.x), -50)
+	
+	var height_above_player = 200
+	missile.position = Vector2(randf_range(player.position.x - 100, player.position.x + 100), player.position.y - height_above_player - randf_range(0, 100))
 	add_child(missile)
 
 
 func _on_veto_hand_timer_timeout() -> void:
-	var screen_size = get_viewport_rect().size
+	var player = $Player
 	var hand = VETO_HAND_SCENE.instantiate()
-	hand.position = Vector2(randf_range(0, screen_size.x), -300)
+	var height_above_player = 300
+	hand.position = Vector2(player.position.x, player.position.y - height_above_player)
 	add_child(hand)
 	var play_hand = hand.get_node("AnimationPlayer")
 	play_hand.play("slam")
-	
 	await play_hand.animation_finished
-	
 	hand.queue_free()
 
 
