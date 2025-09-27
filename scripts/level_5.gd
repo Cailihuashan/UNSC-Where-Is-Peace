ee3d921fedc720e7a03708e8c5567da141a21753
Cailihuashan <thetruegamer404@gmail.com>
2025-09-27 19:20:36 -0400

    Last hack of the day

--- scripts/level_5.gd
+++ scripts/level_5.gd
@@ -5,23 +5,22 @@ const VETO_HAND_SCENE = preload("res://scenes/veto_hand.tscn")
 
 
 func _on_missile_timer_timeout():
-	var player = $Player
+	var screen_size = get_viewport_rect().size
 	var missile = MISSILE_SCENE.instantiate()
-	
-	var height_above_player = 200
-	missile.position = Vector2(randf_range(player.position.x - 100, player.position.x + 100), player.position.y - height_above_player - randf_range(0, 100))
+	missile.position = Vector2(randf_range(0, screen_size.x), -50)
 	add_child(missile)
 
 
 func _on_veto_hand_timer_timeout() -> void:
-	var player = $Player
+	var screen_size = get_viewport_rect().size
 	var hand = VETO_HAND_SCENE.instantiate()
-	var height_above_player = 300
-	hand.position = Vector2(player.position.x, player.position.y - height_above_player)
+	hand.position = Vector2(randf_range(0, screen_size.x), -300)
 	add_child(hand)
 	var play_hand = hand.get_node("AnimationPlayer")
 	play_hand.play("slam")
+	
 	await play_hand.animation_finished
+	
 	hand.queue_free()
 
 
--- scripts/veto_hand.gd
+++ scripts/veto_hand.gd
@@ -1,18 +1,14 @@
 extends Area2D
 
-var tracking_speed = 200.0
-var player: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	player = get_tree().get_first_node_in_group("player")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if player:
-		var direction = sign(player.position.x - position.x)
-		position.x += direction * tracking_speed * delta
+	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
