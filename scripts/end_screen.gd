5cebf28bc8383c1a147effd6a6e100318a41e9dd
Cailihuashan <thetruegamer404@gmail.com>
2025-09-27 15:51:24 -0400

    Made adjustments to the starting and ending screen
    Got rid of the overlaps and and adjusted the texts and more

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -3,14 +3,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(2.0).timeout
+	await get_tree().create_timer(1.0).timeout
 	$Explanation.visible = true
-
 	await get_tree().create_timer(10.0).timeout
+	$Explanation.visible = false
 	$"For countries".visible = true
-
 	await get_tree().create_timer(10.0).timeout
+	$"For countries".visible = false
 	$"Against countries".visible = true
-
 	await get_tree().create_timer(5).timeout
+	$"Against countries".visible = false
 	$Vetoed.visible = true
